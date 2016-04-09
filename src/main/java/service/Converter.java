97be72520297ddab258c8a307eb010c333544a36
Dmitry Budnik <dmitry_budnik@i.ua>
2016-04-09 18:42:56 +0300

    toJson correct

--- src/main/java/service/Converter.java
+++ src/main/java/service/Converter.java
@@ -11,11 +11,10 @@ import java.time.format.DateTimeFormatter;
 
 public class Converter {
     public String toJson(Object object) throws IllegalAccessException {
+        boolean flag = false;
         Class ob = object.getClass();
         Field[] fields = ob.getDeclaredFields();
-        //Annotation[] annotations = ob.getAnnotations();
         String json = "{\n";
-
         for (int i = 0; i < fields.length; i++) {
             Annotation[] annotations = fields[i].getAnnotations();
             if (annotations.length == 0) {
@@ -30,30 +29,30 @@ public class Converter {
                 for (Annotation annotation : annotations) {
                     if (annotation.annotationType().equals(JasonValue.class)) {
                         JasonValue jasonValue = fields[i].getAnnotation(JasonValue.class);
-                        json += "\"" + jasonValue.name() + "\"" + " : " + "\"" + fields[i].get(object);
-                        if (i == fields.length - 1) {
-                            json += "\"\n";
-                        } else {
-                            json += "\",\n";
-                        }
-                    } else if (annotation.annotationType().equals(CustomDateFormat.class)) {
+                        json += "\"" + jasonValue.name() + "\"" + " : " + "\"";
+                    }
+                    if (annotation.annotationType().equals(CustomDateFormat.class)) {
                         LocalDate date = (LocalDate) fields[i].get(object);
                         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(fields[i].getAnnotation(CustomDateFormat.class).format());
                         String format = formatter.format(date);
-                        json += "\"" + fields[i].getName() + "\"" + " : " + "\"" + format;
-                        if (i == fields.length - 1) {
-                            json += "\"\n";
-                        } else {
-                            json += "\",\n";
-                        }
+                        json += format;
+                        flag = true;
                     }
                 }
-            }
+                if (!flag) json += fields[i].get(object);
 
+                if (i == fields.length - 1) {
+                    json += "\"\n";
+                } else {
+                    json += "\",\n";
+                }
+            }
         }
         json += "}";
         return json;
     }
+
+
 
     public static void main(String[] args) throws IllegalAccessException {
         Human human = new Human("1", LocalDate.now());
